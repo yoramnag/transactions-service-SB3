a3ba792f0b08748f9029de36ee6fc3f02819fe27
yoram <yoramnag@gmail.com>
2024-07-06 23:00:36 +0300

    fix TransactionsConstants

--- src/main/java/com/example/transactions/constants/TransactionsConstants.java
+++ src/main/java/com/example/transactions/constants/TransactionsConstants.java
@@ -6,7 +6,7 @@ public class TransactionsConstants {
     }
 
     public static final String  STATUS_201 = "201";
-    public static final String  MESSAGE_201 = "Credit card created successfully";
+    public static final String  MESSAGE_201 = "transaction created successfully";
     public static final String  STATUS_200 = "200";
     public static final String  MESSAGE_200 = "Request processed successfully";
 }
