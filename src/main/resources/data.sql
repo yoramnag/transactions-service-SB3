113b6affcb425b8e8c1a8feb31526d72122fb48d
yoram <yoramnag@gmail.com>
2024-10-21 14:49:22 +0300

    update data.sql and application.properties

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,8 @@
-insert into TRANSACTIONS(transaction_Id , credit_card , date ,amount,mask_credit_card) values(20001 , '89474490a51a4906a89d5ffc873bfe97d5e695a75c8a8b1ae04b4dc44915f22e',PARSEDATETIME('07-07-2022-00.00.00','dd-MM-yyyy-HH.mm.ss'),100.00,'XXXXXXXXXXXX7797');
-insert into TRANSACTIONS(transaction_Id , credit_card , date ,amount,mask_credit_card) values(20002 , '89474490a51a4906a89d5ffc873bfe97d5e695a75c8a8b1ae04b4dc44915f22e',PARSEDATETIME('08-07-2022-00.00.00','dd-MM-yyyy-HH.mm.ss'),300.00,'XXXXXXXXXXXX7797');
+CREATE TABLE IF NOT EXISTS `TRANSACTIONS` (
+  `transaction_Id` int NOT NULL AUTO_INCREMENT,
+  `credit_card` varchar(255) NOT NULL,
+  `amount` FLOAT NOT NULL,
+  `date` date NOT NULL,
+  `mask_credit_card` varchar(255) NOT NULL,
+  PRIMARY KEY (`transaction_Id`)
+);
