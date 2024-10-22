2a6c53ecf1902e4c3c643ef03f5b4438245f9404
yoram <yoramnag@gmail.com>
2024-10-22 10:35:31 +0300

    update application.properties and removing data.sql

--- src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS `TRANSACTIONS` (
-  `transaction_Id` int NOT NULL AUTO_INCREMENT,
-  `credit_card` varchar(255) NOT NULL,
-  `amount` FLOAT NOT NULL,
-  `date` date NOT NULL,
-  `mask_credit_card` varchar(255) NOT NULL,
-  PRIMARY KEY (`transaction_Id`)
-);
